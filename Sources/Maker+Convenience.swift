443407069554522fe47c18769665252371b8e18e
Stanislav Klyukhin <stanislavak314@gmail.com>
2020-02-13 17:35:35 +0600

    Add documentation

--- Sources/Maker+Convenience.swift
+++ Sources/Maker+Convenience.swift
@@ -6,11 +6,29 @@ import UIKit
 
 public extension Maker {
 
+    /// Finds relation with max value
+    ///
+    /// Use this method when you want to find relation with max value.
+    ///
+    /// - parameter lhs:   The left hand side relation to campare.
+    /// - parameter rhs:   The right hand side relation to campare.
+    ///
+    /// - returns: `RelationView<Relation>` relation with max value between `lhs`and `rhs`.
+
     func maxRelation<Relation>(_ lhs: RelationView<Relation>, _ rhs: RelationView<Relation>) -> RelationView<Relation> {
         let lhsConvertedValue = convertedValue(for: lhs)
         let rhsConvertedValue = convertedValue(for: rhs)
         return lhsConvertedValue > rhsConvertedValue ? lhs : rhs
     }
+
+    /// Finds relation with min value
+    ///
+    /// Use this method when you want to find relation with min value.
+    ///
+    /// - parameter lhs:   The left hand side relation to campare.
+    /// - parameter rhs:   The right hand side relation to campare.
+    ///
+    /// - returns: `RelationView<Relation>` relation with min value between `lhs`and `rhs`.
 
     func minRelation<Relation>(_ lhs: RelationView<Relation>, _ rhs: RelationView<Relation>) -> RelationView<Relation> {
         let lhsConvertedValue = convertedValue(for: lhs)
